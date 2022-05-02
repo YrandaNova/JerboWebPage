959ac625b2d6963debbfa41da83bd4e75ada1e6a
YrandaNova <72846683+YrandaNova@users.noreply.github.com>
2022-05-02 17:27:54 -0500

    commit

---
+++ jerboforms.php
@@ -0,0 +1,43 @@
+<?php
+$servername="10.124.0.2";
+$username="root";
+$password="Jerbo401*";
+$dbname="Jerbo";
+
+
+$Name=$_POST['Name']
+$Lastname=$_POST['Lastname']
+$Age=$_POST['Age']
+$Language=$_POST['Language']
+$Country=$_POST['Country']
+$Gender=$_POST['Gender']
+$Education=$_POST['Education']
+$MusicG=$_POST['MusicG']
+$Protection=$_POST['Protection']
+$Salud=$_POST['Salud']
+
+
+
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+if($conn-> connect_error){
+    die ("connection failed: ". $conn->connect_error);
+}else{
+    echo "Registering Answers";
+    $stmt= $conn->prepare("insert into infousuarios(name,lastname,age,language,country,gender,education,musicgenre,protection,health) values(?,?,?,?,?,?,?,?,?,?,?)")
+    $stmt->bind_param("ssisssssss",$Name,$Lastname,$Age,$Language,$Country,$Gender,$Education,$MusicG,$Protection,$Salud );
+    stmt->execute();
+    echo "the answers have been registered"
+    $stmt->close();
+    $conn->close();
+    
+      
+
+}
+
+
+
+
+
+
+?>
